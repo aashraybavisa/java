48626904600b9b1c7ed69eff892a791c3141b1b2
aashraybavisa <aashray1042@gmail.com>
2024-12-30 06:26:07 -0500

    oop: Resistor App Tester

--- OOP/Resistor/ResistorTester.java
+++ OOP/Resistor/ResistorTester.java
@@ -2,14 +2,23 @@ package OOP.Resistor;
 
 public class ResistorTester {
 
+  // 330 Ω ±10 percent resistors
+  public static final double NOMINAL_RESISTANCE = 330;
+  public static final double TOLERANCE = 10;
+
   public static void main(String[] args) {
+    System.out.printf(
+      "\nNominal Resistance - %f, Tolerance - %f\n\n",
+      NOMINAL_RESISTANCE,
+      TOLERANCE
+    );
     for (int i = 1; i <= 10; i++) {
-      Resistor reg = new Resistor(330, 10);
+      Resistor resistor = new Resistor(NOMINAL_RESISTANCE, TOLERANCE);
 
       System.out.printf(
         "Resistor - %2d\nActualResistance - %.2f\n\n",
         i,
-        reg.getActualResistance()
+        resistor.getActualResistance()
       );
     }
   }
